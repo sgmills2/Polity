bdb580cec3e4d322e95505c57647e5020767b3fc
sgmills2 <sgmills2@gmail.com>
2024-12-03 12:48:13 -0600

    index fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles'; // Import ThemeProvider
+import theme from './theme'; // Import the theme file you created
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
